0e93b602b9904cd509549e8006717322ee2cf944
ArRegulus <darkinblade974@hotmail.fr>
2022-03-30 21:59:46 +0200

    update arduino

--- arduino/spyardu.ino
+++ arduino/spyardu.ino
@@ -1,56 +1,69 @@
 #define servo1 2
-//#define servo2 3
-//#define servo3 12
+//#define servo2
+//#define servo3
+//#define borneIN1
+//#define borneIN2
+//#define borneIN3
+//#define borneIN4
+//#define borneENA
+//#define borneENB
 
 int poservo1 = 80;
 //int poservo2 = 40;
 //int poservo3 = 180;
 
 #include <Servo.h>
-Servo monServo1;
-//Servo monServo2;
-//Servo monServo3;
+Servo Servo1;//Servo  Horizontal
+//Servo Servo2; //Servo Vertical
+//Servo Servo3; //Servo chargeur
 
 void setup() {
 	Serial.begin(9600);
-	monServo1.attach(servo1); monServo1.write(poservo1);
-	//monServo2.attach(servo2); monServo2.write(poservo2);
-	//monServo3.attach(servo3); monServo3.write(poservo3);
+	Servo1.attach(servo1); Servo1.write(poservo1);
+	//Servo2.attach(servo2); Servo2.write(poservo2);
+	//Servo3.attach(servo3); Servo3.write(poservo3);
 	delay(10);
+//	pinMode(borneIN1, OUTPUT); pinMode(borneIN2, OUTPUT); //Moteur 1
+//	pinMode(borneIN3, OUTPUT); pinMode(borneIN4, OUTPUT); //Moteur 2
+//	pinMode(borneENA, OUTPUT); pinMode(borneENB, OUTPUT);
 	}
 void loop(){
 	if (Serial.available(){
 		byte nr = Serial.read();
-		Serial.print("The following char was received:");
+		Serial.print("Syst send:");
 		Serial.println(nr, DEC);
 	switch (nr){
-		case 1:
-			digitalWrite(borneIN1, HIGH); digitalWrite(borneIN2, LOW);
-			digitalWrite(borneIN3, LOW); digitalWrite(borneIN4, HIGH);
-			analogWrite(borneENA, 250); analogWrite(borneENB, 250);
-			monservo3.write(280);
+		case shoot://Le  programme a déjà aligné le système et la cible et ordonne de tirer
+			digitalWrite(borneIN1, HIGH);
+			digitalWrite(borneIN2, LOW);
+			digitalWrite(borneIN3, LOW);
+			digitalWrite(borneIN4, HIGH);
+			analogWrite(borneENA, 250);
+			analogWrite(borneENB, 250); //On allume les moteurs
+			delay(10) //On attend que les moteurs soit à  la bonne vitesse
+			servo3.write(280); //On envoie la munition
 			break;
-		case up:
+		case up: // Le programme ordonne de monter
 			poservo1++;
-			monservo1.write(poservo1);
+			servo1.write(poservo1);
 			delay(10);
 			break;
-		case down:
+		case down: // Le programme ordonne de descendre
 			poservo1--;
-			monservo1.write(poservo1);
+			servo1.write(poservo1);
 			delay(10);
 			break;
-		case right:
+		case right: // Le programme ordonne d'orienter vers la droite
 			poservo2++;
-			monservo2.write(poservo2);
+			servo2.write(poservo2);
 			delay(10);
 			break;
-		case left:
+		case left: // Le programme ordonne d'orienter vers la gauche
 			poservo2--;
-			monservo2.write(poservo2);
+			servo2.write(poservo2);
 			delay(10);
 			break;
-		default:
+		default: // cas par défaut, si le programme principal n'envoie rien on arrive ici
 			break;
 		}
 
