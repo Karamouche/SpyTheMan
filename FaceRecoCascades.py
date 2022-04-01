9fd246d3a98bc3d0abfa1d94c2dbab16d4686a59
Karamouche <hugo4ibt@gmail.com>
2022-04-01 11:41:44 +0200

    Detect just face

--- FaceRecoCascades.py
+++ FaceRecoCascades.py
@@ -10,18 +10,20 @@ import cv2
 
 def hasMask(rec):
     face = cv2.CascadeClassifier('cascades/frontalface_alt.xml')
-    mouth = cv2.CascadeClassifier('cascades/Mouth.xml')
+    #mouth = cv2.CascadeClassifier('cascades/Mouth.xml')
     grayRec = cv2.cvtColor(rec, cv2.COLOR_BGR2GRAY)
     faces = face.detectMultiScale(grayRec, 1.1, 4)
-    mouths = mouth.detectMultiScale(grayRec, 1.1, 4)
+    #mouths = mouth.detectMultiScale(grayRec, 1.1, 4)
     
     for (x, y, w, h) in faces:
         cv2.rectangle(rec, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    """
     for (x, y, w, h) in mouths:
         cv2.rectangle(rec, (x, y), (x+w, y+h), (0, 255, 0), 2)
     if len(mouths) <= 3:
         cv2.putText(rec, "MASK", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-    elif faces != ():
+    """
+    if faces != ():
         cv2.putText(rec, "NO MASK", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
     else:
         cv2.putText(rec, "Error", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
