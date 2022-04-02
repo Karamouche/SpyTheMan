9a8ca4193792eb5a5d2848bb8a465992b08e15da
Karamouche <hugo4ibt@gmail.com>
2022-04-02 19:43:55 +0200

    cascade update

--- FaceRecoCascades.py
+++ FaceRecoCascades.py
@@ -9,22 +9,25 @@ Small algorithme to get if a mask is wear or not
 import cv2
 
 def hasMask(rec):
+    """
+    If a face is detect, and the mouth inside of it, there is no mask -> shoot
+    If a face is detect, possibility to wear a mask
+    else, nothing
+    """
     face = cv2.CascadeClassifier('cascades/frontalface_alt.xml')
-    #mouth = cv2.CascadeClassifier('cascades/Mouth.xml')
+    mouth = cv2.CascadeClassifier('cascades/mouth.xml')
     grayRec = cv2.cvtColor(rec, cv2.COLOR_BGR2GRAY)
     faces = face.detectMultiScale(grayRec, 1.1, 4)
-    #mouths = mouth.detectMultiScale(grayRec, 1.1, 4)
+    mouths = mouth.detectMultiScale(grayRec, 1.1, 10)
     
     for (x, y, w, h) in faces:
         cv2.rectangle(rec, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    """
     for (x, y, w, h) in mouths:
         cv2.rectangle(rec, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    if len(mouths) <= 3:
+    if len(mouths) <= 3: //MOUTH INSIDE FACE
         cv2.putText(rec, "MASK", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-    """
     if faces != ():
-        cv2.putText(rec, "NO MASK", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(rec, "NO FACE", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
     else:
         cv2.putText(rec, "Error", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
     return rec
@@ -44,5 +47,5 @@ while cam.isOpened():
     if cv2.waitKey(1) == 27:
         break
     
-cv2.destroyWindow("TheCam")
+cv2.destroyWindow("SpyTheMan")
 cam.release()
