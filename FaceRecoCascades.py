d269a413efb1e5f162076a776ed5e1fbd14d53d3
Karamouche <hugo4ibt@gmail.com>
2022-04-06 10:56:37 +0200

    Update FaceRecoCascades.py

--- FaceRecoCascades.py
+++ FaceRecoCascades.py
@@ -15,28 +15,29 @@ def hasMask(rec):
     else, nothing
     """
     face = cv2.CascadeClassifier('cascades/frontalface_alt.xml')
-    mouth = cv2.CascadeClassifier('cascades/mouth_alt.xml')
+    #mouth = cv2.CascadeClassifier('cascades/mouth_alt.xml')
     grayRec = cv2.cvtColor(rec, cv2.COLOR_BGR2GRAY)
-    faces = face.detectMultiScale(grayRec, 1.1, 4)
-    mouths = mouth.detectMultiScale(grayRec, 1.1, 10)
-    
+    faces = face.detectMultiScale(grayRec, 1.1, 5, minSize=(30,30), flags=cv2.CASCADE_SCALE_IMAGE)
+    #mouths = mouth.detectMultiScale(grayRec, 1.1, 4)
     for (x, y, w, h) in faces:
         cv2.rectangle(rec, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    """
     for (x, y, w, h) in mouths:
         cv2.rectangle(rec, (x, y), (x+w, y+h), (0, 255, 0), 2)
     if len(mouths) <= 3: #MOUTH INSIDE FACE
         cv2.putText(rec, "MASK", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-    if faces != ():
+    elif faces != ():
         cv2.putText(rec, "NO FACE", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
     else:
         cv2.putText(rec, "Error", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
+    """
     return rec
     
 
 cam = cv2.VideoCapture(0)
 succes, rec = cam.read()
 rec = cv2.flip(rec, 1)
-faceClassifier = cv2.CascadeClassifier('cascades/haarcascade_frontalface_alt.xml')
+rec = cv2.resize(rec, (1280, 720))
 if not cam.isOpened():
     print("Error initializing camera")
 
@@ -44,6 +45,7 @@ while cam.isOpened():
     cv2.imshow("SpyTheMan", hasMask(rec))
     succes, rec = cam.read()
     rec = cv2.flip(rec, 1)
+    rec = cv2.resize(rec, (1280, 720))
     if cv2.waitKey(1) == 27:
         break
     
