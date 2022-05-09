b459449fd21ca0691a6e09c81a4814ccabc58188
ArRegulus <darkinblade974@hotmail.fr>
2022-05-09 17:44:33 +0100

    update arduino control

--- arduino/ardControl.py
+++ arduino/ardControl.py
@@ -1,10 +1,9 @@
 import serial 
 channel = serial.Serial('dev/ttyACM0',9600) #Complete le port serial ls /dev/tty*
-options = ['shoot','up','down','right','left']
+options = ['0','1','2','3','4']
 while True:
-    message = input("Choose between : shoot/up/down/right/left")
+    message = input("shoot : 0/up : 1/down : 2/right : 3/left : 4")
     for i in options:
         if message == i:
             channel.write(message)
             print('Message send :',message)
-            
--- arduino/spyardu.ino
+++ arduino/spyardu.ino
@@ -1,12 +1,10 @@
-//#define servo1 2
+//#define servo1 
 //#define servo2
 //#define servo3
-//#define borneIN1
-//#define borneIN2
-//#define borneIN3
-//#define borneIN4
-//#define borneENA
-//#define borneENB
+#define borneIN1 2
+#define borneIN2 3
+#define borneIN3 4
+#define borneIN4 5
 
 int poservo1 = 80;
 //int poservo2 = 40;
@@ -29,43 +27,38 @@ void setup() {
 	}
 void loop(){
 	if (Serial.available()){
-		byte nr = Serial.read();
+		byte message  = Serial.read();
 		Serial.print("Syst send:");
-		Serial.println(nr, DEC);
+		Serial.println(message, DEC);
 		}
-	switch (nr){
-		case shoot://Le  programme a déjà aligné le système et la cible et ordonne de tirer
-			digitalWrite(borneIN1, HIGH);
-			digitalWrite(borneIN2, LOW);
-			digitalWrite(borneIN3, LOW);
-			digitalWrite(borneIN4, HIGH);
-			analogWrite(borneENA, 250);
-			analogWrite(borneENB, 250); //On allume les moteurs
-			delay(10) //On attend que les moteurs soit à  la bonne vitesse
-			servo3.write(280); //On envoie la munition
-			break;
-		case up: // Le programme ordonne de monter
-			poservo1++;
-			servo1.write(poservo1);
-			delay(10);
-			break;
-		case down: // Le programme ordonne de descendre
-			poservo1--;
-			servo1.write(poservo1);
-			delay(10);
-			break;
-		case right: // Le programme ordonne d'orienter vers la droite
-			poservo2++;
-			servo2.write(poservo2);
-			delay(10);
-			break;
-		case left: // Le programme ordonne d'orienter vers la gauche
-			poservo2--;
-			servo2.write(poservo2);
-			delay(10);
-			break;
-		default: // cas par défaut, si le programme principal n'envoie rien on arrive ici
-			break;
+	if (message ==  0){		//Le  programme a déjà aligné le système et la cible et ordonne de tirer
+		digitalWrite(borneIN1, HIGH);
+		digitalWrite(borneIN2, LOW);
+		digitalWrite(borneIN3, LOW);
+		digitalWrite(borneIN4, HIGH);
+		analogWrite(borneENA, 250);
+		analogWrite(borneENB, 250); //On allume les moteurs
+		delay(10) //On attend que les moteurs soit à  la bonne vitesse
+		servo3.write(280); //On envoie la munition
+		}
+	else if (message == 1){		 // Le programme ordonne de monter
+		poservo1++;
+		servo1.write(poservo1);
+		delay(10);
+		}
+	else if (message == 2){			 // Le programme ordonne de descendre
+		poservo1--;
+		servo1.write(poservo1);
+		delay(10);
+	else if (message == 3){			 // Le programme ordonne d'orienter vers la droite
+		poservo2++;
+		servo2.write(poservo2);
+		delay(10);
+	else if (message == 4){ // Le programme ordonne d'orienter vers la gauche
+		poservo2--;
+		servo2.write(poservo2);
+		delay(10);
+	
 	}
 }
 
